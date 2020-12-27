c7263f94b000ab10795cc5e89545bf918e532110
jcgsr <cabralguerra@gmail.com>
2020-12-27 09:28:15 -0300

    contar tentativas

--- app.js
+++ app.js
@@ -44,20 +44,23 @@ document.addEventListener('DOMContentLoaded', () => {
       {
          name: 'sad6',
          img: 'img/sad6.jpg'
-      },   
+      },
       {
          name: 'sad6',
          img: 'img/sad6.jpg'
-      }   
+      }
    ]
 
    cardArray.sort(() => 0.5 - Math.random())
 
    const grid = document.querySelector('.grid')
    const resultDisplay = document.querySelector("#result")
+   const triesDisplay = document.querySelector("#tries")
    var cardsChosen = []
    var cardsChosenId = []
    var cardsWon = []
+   var count = 0
+   
 
    // create you board
    function createBoard() {
@@ -79,28 +82,36 @@ document.addEventListener('DOMContentLoaded', () => {
          alert("Achou um par!")
          cards[optionOneId].setAttribute('src', 'img/gray.jpg')
          cards[optionTwoId].setAttribute('src', 'img/gray.jpg')
+         cards[optionOneId].removeEventListener("click", flipCard);
+         cards[optionTwoId].removeEventListener("click", flipCard);
          cardsWon.push(cardsChosen)
+         count += 1
+
       } else {
          cards[optionOneId].setAttribute('src', 'img/purple.jpg')
-         cards[optionTwoId].setAttribute('src', 'img/purple.jpg')
-         alert("Nada feito... Tente de novo.")
+         cards[optionTwoId].setAttribute('src', 'img/purple.jpg')  
+         count += 1          
       }
+
       cardsChosen = []
       cardsChosenId = []
       resultDisplay.textContent = cardsWon.length
-      if (cardsWon.length === cardsArray.length / 2) {
-         resultDisplay.textContent = "Parabéns! Você achou tudo! "
+      triesDisplay.textContent = count
+      if (cardsWon.length === cardArray.length / 2) {
+         resultDisplay.textContent = "Parabéns! Você achou os " + cardsWon.length + " pares com " + count + " tentativas."
       }
    }
 
    // flip your card
    function flipCard() {
       var cardId = this.getAttribute('data-id')
-      cardsChosen.push(cardArray[cardId].name)
-      cardsChosenId.push(cardId)
-      this.setAttribute('src', cardArray[cardId].img)
-      if (cardsChosen.length === 2) {
-         setTimeout(checkForMatch, 500);
+      if (!cardsChosenId.includes(cardId)) {
+         cardsChosen.push(cardArray[cardId].name)
+         cardsChosenId.push(cardId)
+         this.setAttribute('src', cardArray[cardId].img)
+         if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500);
+         }
       }
    }
    createBoard()
@@ -108,9 +119,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 let date = new Date()
 let currentDate = date.toLocaleDateString('pt-BR', {
-    day: '2-digit',
-    month: 'short',
-    year: 'numeric'
+   day: '2-digit',
+   month: 'short',
+   year: 'numeric'
 })
 
 document.querySelector('p#date').innerHTML = currentDate
